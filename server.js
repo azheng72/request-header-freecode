de436339fe444d536a726f8dc0507cc6b710e990
Zheng <williamsjr.nexus@gmail.com>
2016-08-05 06:02:13 +0000

    finish

---
+++ server.js
@@ -0,0 +1,10 @@
+var express=require("express");
+var app=express();
+app.get("",function(req,res){
+    var result={ipaddress:req.ip,language:req.headers["accept-language"],sofeware:req.headers["user-agent"]};
+    //console.log();
+    res.json(result);
+});
+app.listen(process.env.PORT ||8080,function(){
+    console.log("listen to port : process.env.PORT ||8080");
+});
