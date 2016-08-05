fb8622b3760a8e1cb6d77de0342d60a6e6f56a15
Zheng <williamsjr.nexus@gmail.com>
2016-08-05 06:11:13 +0000

    add end()

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ app.get("",function(req,res){
     var result={ipaddress:req.ip,language:req.headers["accept-language"],sofeware:req.headers["user-agent"]};
     //console.log();
     res.json(result);
+    res.end();
 });
 app.listen(process.env.PORT ||8080,function(){
     console.log("listen to port : process.env.PORT ||8080");
